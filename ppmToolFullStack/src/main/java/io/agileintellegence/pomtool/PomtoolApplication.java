2a01f1a35fe0f234e662b63f493440cc80b52017
Sabuhi Yusifov <sayusi@ttu.ee>
2018-11-21 02:28:08 +0200

    first commit

---
+++ ppmToolFullStack/src/main/java/io/agileintellegence/pomtool/PomtoolApplication.java
@@ -0,0 +1,12 @@
+package io.agileintellegence.pomtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomtoolApplication.class, args);
+    }
+}
